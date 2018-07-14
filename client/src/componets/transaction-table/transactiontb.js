a07c6a136cadb09073b2b0a0e18d9584472474d0
esteban <esteban_armendariz@hotmail.com>
2018-07-14 14:03:29 -0500

    set up routers and separate components

--- client/src/componets/transaction-table/transactiontb.js
+++ client/src/componets/transaction-table/transactiontb.js
@@ -4,8 +4,14 @@ import {Row, Icon} from 'react-materialize'
 import './transaction.css'
 import TableInput from './tableInput/tableinput'
 
-// class TransactionTable extends Component {
-    // render(){
+import axios from 'axios';
+
+
+
+const API_URL = 'http://localhost:8080/api';
+
+class TransactionTable extends Component {
+    
        
 
         // const sampleData = [
@@ -14,38 +20,68 @@ import TableInput from './tableInput/tableinput'
         //     { date: '7/1/2018', description: 'Target',category: 'Cleaning Suppys',cost: 41.47 ,isEditing:false }
         // ];
 
-        const data = (props) => {
-            
-        return(
-            <div className="row">
-                <div className="col s8 offset-s2">
-                    <table className="striped s6 offset-s6">
-                        <thead>
-                            <tr>
-                                <th> Date  </th>
-                                <th> Description  </th>
-                                <th> category  </th>
-                                <th> Cost  </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {props.data.map(tranaction => (
-                            <tr key={tranaction._id}  className={ tranaction.isEditing ? 'background-active' : 'test'} >
-                                <td>  <TableInput name="test" value={tranaction.description} isEditing={tranaction.isEditing} /> {tranaction.transaction.transactionDate}</td>
-                                <td>  {tranaction.product.productName} </td>
-                                <td className="borders"> <input className="input-bottom" type="text" value={tranaction.category} /> {tranaction.product.productDepartment} </td>
-                                <td> ${tranaction.transac} </td>
-                            </tr>  
-                        ))}
-                        </tbody>   
-                    </table> 
-            </div>
-            </div>
-        // }
+
+        
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      data:[],
+     
+    };
+  };
+
+  componentDidMount(){
+        axios
+            .get(API_URL)
+            .then(response => {
+                this.setState({
+                data: response.data
+                });
+                console.log(response.data)
+
+            })
+            .catch((err)=> {
+            console.log(err)
+        })
+    };
+
+    // const data = (props) => {
        
-        )
-    }
+    render(){
+    return(
+        <div className="row">
+            <div className="col s8 offset-s2">
+                <table className="striped s6 offset-s6">
+                    <thead>
+                        <tr>
+                            <th> Date  </th>
+                            <th> Description  </th>
+                            <th> category  </th>
+                            <th> Cost  </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.data.map(tranaction => (
+                        <tr key={tranaction._id}  className={ tranaction.isEditing ? 'background-active' : 'test'} >
+                            <td>  <TableInput name="test" value={tranaction.description} isEditing={tranaction.isEditing} /> {tranaction.transaction.transactionDate}</td>
+                            <td>  {tranaction.product.productName} </td>
+                            <td className="borders"> <input className="input-bottom" type="text" value={tranaction.category} /> {tranaction.product.productDepartment} </td>
+                            <td> ${tranaction.transaction.transactionTotal} </td>
+                        </tr>  
+                    ))}
+                    </tbody>   
+                </table> 
+        </div>
+        </div>
+    // }
+    
+    )
+}
+}
+
+
 //     }
 // }
 
-export default data;
+export default TransactionTable;
