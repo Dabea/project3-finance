7f9cbd380346206878f949dab8adf53d61ba6f4f
Abraham <abeis@msn.com>
2018-07-13 23:53:53 -0500

    added input componet

--- client/src/componets/transaction-table/transactiontb.js
+++ client/src/componets/transaction-table/transactiontb.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { debug } from 'util';
 import {Row, Icon} from 'react-materialize'
+import './transaction.css'
+import TableInput from './tableInput/tableinput'
 
 class TransactionTable extends Component {
     render(){
+       
+
         const sampleData = [
-            { date: '7/1/2018', description: 'Rent', category: 'Houseing', cost: 1200 },
-            { date: '7/1/2018', description: 'McDonalds',category: 'Fast Food',cost: 12.39},
-            { date: '7/1/2018', description: 'Target',category: 'Cleaning Suppys',cost: 41.47}
+            { date: '7/1/2018', description: 'Rent', category: 'Houseing', cost: 1200.00  ,isEditing:false },
+            { date: '7/1/2018', description: 'McDonalds',category: 'Fast Food',cost: 12.39 ,isEditing:false },
+            { date: '7/1/2018', description: 'Target',category: 'Cleaning Suppys',cost: 41.47 ,isEditing:false }
         ];
 
         return(
@@ -24,11 +28,11 @@ class TransactionTable extends Component {
                         </thead>
                         <tbody>
                         {sampleData.map( (tranaction) => 
-                            <tr>
-                                <td> {tranaction.date} </td>
+                            <tr  className={ tranaction.isEditing ? 'background-active' : 'test'} >
+                                <td>  <TableInput name="test" value={tranaction.description} isEditing={tranaction.isEditing} /></td>
                                 <td>  {tranaction.description} </td>
-                                <td className="borders">  {tranaction.category} </td>
-                                <td>  {tranaction.cost} </td>
+                                <td className="borders"> <input className="input-bottom" type="text" value={tranaction.category} /> </td>
+                                <td> ${tranaction.cost} </td>
                             </tr>  
                         )}
                         </tbody>   
