29aed757f3273916292d6864d1f919f7990aede2
osman144 <osman144@umn.edu>
2018-07-17 21:45:35 -0500

    set up models

--- models/Product.js
+++ models/Product.js
@@ -1,33 +1,80 @@
 var mongoose = require('mongoose');
 
 var productSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-    product: {
-		productName: {
-			type: String,
-			required: true
-        },
-        productStore: {
-            type: String,
-        },
-        productDepartment: { 
-            type: String
+
+
+    Receipt: {
+        Items: [{
+            Product: {
+                
+                name: {
+                type: String,
+                required: true,
+                },
+                quantity: {
+                    type: String,
+                    required: true
+                },
+                cost: {
+                    type: String,
+                    required: true
+                },
+                category: {
+                    type: String,
+                    required: true
+                }
         }
+    }
+    ], 
+
+    Date: {
+        type: Date,
+        default: Date.now
+    },
+
+    Store: {
+        type: String,
+        required: true
+
+    },
+
+    Total: {
+        type: String,
+        required: true
+
+    }, 
+
+
+
+
+
+    }
+    // product: {
+	// 	productName: {
+	// 		type: String,
+	// 		required: true
+    //     },
+    //     productStore: {
+    //         type: String,
+    //     },
+    //     productDepartment: { 
+    //         type: String
+    //     }
 
         
-	},
-	transaction:{
-        transactionDate:{
-            type: Date,
-            default: Date.now
-        },
-        transactionQuantity: {
-            type: String
-        },
-        transactionTotal: {
-            type: String,
-        },
-    }
+	// },
+	// transaction:{
+    //     transactionDate:{
+    //         type: Date,
+    //         default: Date.now
+    //     },
+    //     transactionQuantity: {
+    //         type: String
+    //     },
+    //     transactionTotal: {
+    //         type: String,
+    //     },
+    // }
 });
 
 var Product = mongoose.model('Product', productSchema);
