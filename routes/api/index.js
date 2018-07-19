12df3ac74c89e2da102a0801b4b8b1e8928b536d
Abraham Anderson <abeis@msn.com>
2018-07-19 10:11:26 -0500

    cleaned up routeing files

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const transactionsRoutes = require("./transactions");
-
-// Book routes
-router.use("/", transactionsRoutes);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const path = require ('path');
 const router = require ('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/transactions');
 
 //======================================
 //======================================
@@ -19,9 +19,6 @@ router.use('/api', apiRoutes);
 router.use('/', function(req,res){
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-
-
 
 module.exports = router 
 
