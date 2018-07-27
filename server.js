ad66a2719afdd0d6aa3d9343728c462ad808dbd4
osman144 <osman144@umn.edu>
2018-07-26 19:00:02 -0500

    fix api routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const express = require("express");
 const bodyParser = require('body-parser')
-// const routes = require ("./routes");
+const routes = require ("./routes");
 const app = express();
 // const mongoose = require('mongoose');
 const morgan = require('morgan')
@@ -52,6 +52,10 @@ if (process.env.NODE_ENV === "production") {
 /* Express app ROUTING */
 app.use('/auth', require('./authentication/auth'))
 
+
+// Use routes
+app.use(routes)
+
 // ====== Error handler ====
 app.use(function(err, req, res, next) {
 	console.log('====== ERROR =======')
@@ -64,3 +68,32 @@ app.use(function(err, req, res, next) {
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT + "!");
 });
+
+// const express = require("express");
+// const routes = require ("./routes");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const mongoose = require('mongoose');
+// const PORT = process.env.PORT || 3001;
+
+// // Define middleware here
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// // Add routes, both API and view
+// app.use(routes);
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/products');
+
+// app.listen(PORT, function() {
+//   console.log("API " + PORT + "!");
+// });
